3a7311fa8d15aa948fda38497b2e7836deb9d156
Vishal Kumar <vishal.k0905@gmail.com>
2020-10-11 02:57:46 +0530

    Updates

---
+++ precache-manifest.093d7a7a0b3f17329263991224c6ed2e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dac94a9530c43972291b4ac3f8d769f5",
+    "url": "/CRI-fluid-systems/index.html"
+  },
+  {
+    "revision": "4f21b31dc93bb816c2f1",
+    "url": "/CRI-fluid-systems/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "e5122eec7d4ee8ac4d08",
+    "url": "/CRI-fluid-systems/static/css/main.ab1cc257.chunk.css"
+  },
+  {
+    "revision": "4f21b31dc93bb816c2f1",
+    "url": "/CRI-fluid-systems/static/js/2.f65dd4ef.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/CRI-fluid-systems/static/js/2.f65dd4ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5122eec7d4ee8ac4d08",
+    "url": "/CRI-fluid-systems/static/js/main.7db3c538.chunk.js"
+  },
+  {
+    "revision": "86eebddda9997f310130",
+    "url": "/CRI-fluid-systems/static/js/runtime-main.28f7cdf7.js"
+  },
+  {
+    "revision": "29d96123f6fe54ac99593cfc60bbef44",
+    "url": "/CRI-fluid-systems/static/media/1.29d96123.png"
+  },
+  {
+    "revision": "57d5c6509602db21e03912c7a89a79e4",
+    "url": "/CRI-fluid-systems/static/media/2.57d5c650.png"
+  },
+  {
+    "revision": "b5ddb1417b1786fc22028f4c49e4ffe3",
+    "url": "/CRI-fluid-systems/static/media/3.b5ddb141.png"
+  },
+  {
+    "revision": "5762018e4279189fa56715c827648c1c",
+    "url": "/CRI-fluid-systems/static/media/logo.5762018e.png"
+  }
+]);
